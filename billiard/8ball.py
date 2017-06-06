db441ef5b48f03b5f69ae16c0866c9e26d00a5d8
JeongJu Kim <haje01@gmail.com>
2017-06-06 23:59:47 +0900

    done basic dynamics

--- billiard/8ball.py
+++ billiard/8ball.py
@@ -23,44 +23,41 @@ SCALE = 30.0
 ball_to_hit = None
 ball_to_move = None
 mouse_down_pt = None
-max_impulse = 25
+MAX_IMPULSE = 25
 rm = None
 
-shot = 0
-
-canvas_width = 700 # 750
-canvas_height = 385 # 440
-table_height = 385
-ball_radius = 0.4
-cue_start = array((508, 192)) / SCALE
+CANVAS_WIDTH = 700 # 750
+CANVAS_HEIGHT = 385 # 440
+BALL_RADIUS = 0.4
+CUE_START = array((508, 192)) / SCALE
+CUE_LINE_WIDTH = 5
 
 # first ball starting point
-sp = array((192, 191)) / SCALE
+SP = array((192, 191)) / SCALE
 
 # ball exit
-be = array((70, 400)) / SCALE
+BE = array((70, 400)) / SCALE
 
-goal_pts = [array([34, 35]), array([350, 25]), array([664, 35]),
+GOAL_PTS = [array([34, 35]), array([350, 25]), array([664, 35]),
             array([664, 350]), array([350, 360]), array([35, 350])]
 
-for gp in goal_pts:
-    gp = gp / SCALE
+for i, gp in enumerate(GOAL_PTS):
+    GOAL_PTS[i] = gp / SCALE
 
 
-stick_start_color = RGB(r=254, g=232, b=214)
-stick_end_color = RGB(r=255, g=0, b=0)
-stick_distance = 0
+STICK_START_COLOR = array((254, 232, 214))
+STICK_END_COLOR = array((255, 0, 0))
 world = {}
 worker = None
 bodies_state = None
 box = None
 
-clear_color = (0.5, 0.5, 0.5, 1)
+CLEAR_COLOR = (0.5, 0.5, 0.5, 1)
 
 class Entity:
     def __init__(self, id, pos, static_body=False, hidden=False):
         self.id = id
-        self.pos = array(pos)
+        self.pos = array(pos, dtype='float64')
         self.angle = 0
         self.center = 0
         self.restitution = 0.3
@@ -71,6 +68,11 @@ class Entity:
         self.static_body = static_body
         self.color = RGBA(128, 128, 128, 0.5)
         self.hidden = hidden
+
+    def update(self, state):
+        self.pos[0] = state.x
+        self.pos[1] = state.y
+        self.angle = state.angle
 
 
 class CircleEntity(Entity):
@@ -86,7 +88,7 @@ class PolygonEntity(Entity):
     def __init__(self, id, pos, points, static_body, hidden):
         super(PolygonEntity, self).__init__(id, pos, static_body=static_body,
                                             hidden=hidden)
-        self.points = array(points)
+        self.points = array(points, dtype='float64')
 
 
 class RectangleEntity(Entity):
@@ -100,17 +102,18 @@ class RectangleEntity(Entity):
 
 
 class Box:
-    def __init__(self, interval_rate, adaptive, width, height, scale, gravity_y):
-        self.interval_rate = 60
-        self.adaptive = False
-        self.width = 640
-        self.height = 480
-        self.scale = 30
+    def __init__(self, interval_rate=60, adaptive=False, width=640, height=480,
+                 scale=30, gravity_x=0, gravity_y=10):
+        self.interval_rate = interval_rate
+        self.adaptive = adaptive
+        self.width = width
+        self.height = height
+        self.scale = scale
         self.bodies_map = {}
         self.fixtures_map = []
         self.world = None
-        self.gravity_x = 0
-        self.gravity_y = 10
+        self.gravity_x = gravity_x
+        self.gravity_y = gravity_y
         self.allow_sleep = True
         self.world = b2World(b2Vec2(self.gravity_x, self.gravity_y), self.allow_sleep)
 
@@ -125,7 +128,7 @@ class Box:
 
     def get_state(self):
         state = {}
-        for b in self.world.bodies_gen():
+        for i, b in enumerate(self.world.bodies):
             if b.active and b.userData is not None:
                 body = Body(x=b.position.x, y=b.position.y, angle=b.angle,
                             center=Point(x=b.worldCenter.x, y=b.worldCenter.y))
@@ -154,33 +157,34 @@ class Box:
             fix_def.shape = b2CircleShape(radius=entity.radius)
         elif hasattr(entity, 'points'):
             fix_def.shape = b2PolygonShape()
-            vec = b2Vec2()
+            vertices = []
             for i, pt in enumerate(entity.points):
-                vec.Set(float(pt[0]), float(pt[1]))
-                fix_def.shape.set_vertex(i, vec)
+                vec = b2Vec2()
+                vec.Set(pt[0], pt[1])
+                vertices.append(vec)
+            fix_def.shape.vertices = vertices
         else:
             fix_def.shape = b2PolygonShape()
             fix_def.shape.SetAsBox(entity.half_width, entity.half_height)
 
         body_def.position.x = float(entity.pos[0])
         body_def.position.y = float(entity.pos[1])
-        body_def.user_data = entity.id
-        body_def.linear_damping = entity.linear_damping
-        body_def.angular_damping = entity.angular_damping
+        body_def.userData = entity.id
+        body_def.linearDamping = entity.linear_damping
+        body_def.angularDamping = entity.angular_damping
         self.bodies_map[entity.id] = self.world.CreateBody(body_def)
         self.bodies_map[entity.id].CreateFixture(fix_def)
 
 
-    def apply_impulse(body_id, degrees, power):
+    def apply_impulse(self, body_id, degrees, power):
         body = self.bodies_map[body_id]
-        body.ApplyImpulse(b2Vec2(math.cos(degrees * (math.pi / 180)) * power,
-                                 math.sin(degrees * (math.pi / 180)) * power,
-                                 body.GetWorldCenter()))
+        body.ApplyLinearImpulse(b2Vec2(math.cos(degrees * (math.pi / 180)) * power,
+                                      math.sin(degrees * (math.pi / 180)) * power),
+                                      body.worldCenter, True)
 
     def remove_body(self, id):
         if id in self.bodies_map:
             self.world.DestroyBody(self.bodies_map[id])
-            del self.bodies_map[id]
 
 
 def arc_poss(verts, r, start_deg, arc_deg, num_seg):
@@ -230,7 +234,7 @@ class Ball(CircleEntity):
         super(Ball, self).__init__(id, pos)
         color = hex_to_rgb(color)
         self.color = color
-        self.radius = ball_radius
+        self.radius = BALL_RADIUS
         self.striped = striped
         if not striped:
             self.solid_verts = self.make_solid_verts(self.radius * SCALE, color)
@@ -238,33 +242,40 @@ class Ball(CircleEntity):
             self.solid_verts = self.make_solid_verts(self.radius * SCALE,
                                                      [255, 255, 255])
             self.stripe_verts = self.make_stripe_verts(self.radius * SCALE, color)
+        self.text = pyglet.text.Label(str(id), font_size=10,
+                                      color=(255, 255, 255, 255), x=-1, y=1,
+                                      anchor_x='center', anchor_y='center')
 
     def draw(self):
         glPushMatrix()
-        glTranslatef(self.pos[0] * SCALE, self.pos[1] * SCALE, 0)
+        glLoadIdentity()
+        pos = self.pos * SCALE
+        glTranslatef(pos[0], pos[1], 0)
+        glRotatef(self.angle * 180 / math.pi, 0, 0, 1)
         self.solid_verts.draw(GL_TRIANGLE_FAN)
         if self.striped:
             self.stripe_verts.draw(GL_TRIANGLE_FAN)
+        self.text.draw()
         glPopMatrix()
 
 
 eight_ball_locs = [
-    [*cue_start],
-    [*sp],
-    [sp[0] - (2 * ball_radius), sp[1] - ball_radius],
-    [sp[0] - (4 * ball_radius), sp[1] + (2 * ball_radius)],
-    [sp[0] - (6 * ball_radius), sp[1] - (3 * ball_radius)],
-    [sp[0] - (8 * ball_radius), sp[1] + (4 * ball_radius)],
-    [sp[0] - (8 * ball_radius), sp[1] - (2 * ball_radius)],
-    [sp[0] - (6 * ball_radius), sp[1] + ball_radius],
-    [sp[0] - (4 * ball_radius), sp[1]],
-    [sp[0] - (2 * ball_radius), sp[1] + ball_radius],
-    [sp[0] - (4 * ball_radius), sp[1] - (2 * ball_radius)],
-    [sp[0] - (6 * ball_radius), sp[1] + (3 * ball_radius)],
-    [sp[0] - (8 * ball_radius), sp[1] - (4 * ball_radius)],
-    [sp[0] - (8 * ball_radius), sp[1] + (2 * ball_radius)],
-    [sp[0] - (6 * ball_radius), sp[1] - ball_radius],
-    [sp[0] - (8 * ball_radius), sp[1]]
+    [*CUE_START],
+    [*SP],
+    [SP[0] - (2 * BALL_RADIUS), SP[1] - BALL_RADIUS],
+    [SP[0] - (4 * BALL_RADIUS), SP[1] + (2 * BALL_RADIUS)],
+    [SP[0] - (6 * BALL_RADIUS), SP[1] - (3 * BALL_RADIUS)],
+    [SP[0] - (8 * BALL_RADIUS), SP[1] + (4 * BALL_RADIUS)],
+    [SP[0] - (8 * BALL_RADIUS), SP[1] - (2 * BALL_RADIUS)],
+    [SP[0] - (6 * BALL_RADIUS), SP[1] + BALL_RADIUS],
+    [SP[0] - (4 * BALL_RADIUS), SP[1]],
+    [SP[0] - (2 * BALL_RADIUS), SP[1] + BALL_RADIUS],
+    [SP[0] - (4 * BALL_RADIUS), SP[1] - (2 * BALL_RADIUS)],
+    [SP[0] - (6 * BALL_RADIUS), SP[1] + (3 * BALL_RADIUS)],
+    [SP[0] - (8 * BALL_RADIUS), SP[1] - (4 * BALL_RADIUS)],
+    [SP[0] - (8 * BALL_RADIUS), SP[1] + (2 * BALL_RADIUS)],
+    [SP[0] - (6 * BALL_RADIUS), SP[1] - BALL_RADIUS],
+    [SP[0] - (8 * BALL_RADIUS), SP[1]]
 ]
 
 
@@ -319,109 +330,129 @@ def is_point_in_poly(poly, pt):
 
 
 def get_color_fade(start, end, percent):
-    r = math.floor((end.r - start.r) * percent) + start.r
-    g = math.floor((end.g - start.g) * percent) + start.g
-    b = math.floor((end.b - start.b) * percent) + start.b
-    return dict(r=r, g=g, b=b)
+    return ((end - start) * percent).astype(int) + start
 
 
 def intersect(s1, s2, radii_squared):
-    distance_squared = np.linalg.norm(s1, s2)
-    # math.pow(s1.x, s2.x, 2) - math.pow(s1.y - s2.y, 2)
+    distance_squared = np.linalg.norm(s1 - s2)
     return distance_squared < radii_squared
 
 
+def get_gfx_mouse(x, y):
+    return array((x, y)) / SCALE
+
+
 def get_collided_ball(mouse):
-    for i in range(len(initial_state)):
-        if intersect(mouse, world[i], 0.5):
-            return world[i]
+    for k, entity in world.items():
+        if intersect(mouse, entity.pos, 0.5):
+            return entity
     return None
 
 
-def get_degree(center, pt):
-    if center.x == pt.x and center.y == pt.y:
+def get_degrees(center, pt):
+    if np.array_equal(center, pt):
         return 0
-    elif center.x == pt.x:
-        if center.y < pt.y:
+    elif center[0] == pt[0]:
+        if center[1] < pt[1]:
             return 180
         else:
             return 0
-    elif center.y == pt.y:
-        if center.x > pt.x:
+    elif center[1] == pt[1]:
+        if center[0] > pt[0]:
             return 270
         else:
             return 90
-    elif center.x < pt.x and center.y > pt.y:
+    elif center[0] < pt[0] and center[1] > pt[1]:
         # quadrant 1
-        return math.atan((pt.x - center.x)/(center.y - pt.y)) * (180 / math.pi)
-    elif center.x < pt.x and center.y < pt.y:
+        return math.atan((pt[0] - center[0])/(center[1] - pt[1])) * (180 / math.pi)
+    elif center[0] < pt[0] and center[1] < pt[1]:
         # quadrant 2
-        return 90 + math.atan((pt.y - center.y)/(pt.x - center.x)) * (180 / math.pi)
-    elif center.x > pt.x and center.y < pt.y:
+        return 90 + math.atan((pt[1] - center[1])/(pt[0] - center[0])) * (180 / math.pi)
+    elif center[0] > pt[0] and center[1] < pt[1]:
         # quadrant 3
-        return 180 + math.atan((center.x - pt.x)/(pt.y - center.y)) * (180 / math.pi)
+        return 180 + math.atan((center[0] - pt[0])/(pt[1] - center[1])) * (180 / math.pi)
     else:
         # quadrant 4
-        return 270 + math.atan((center.y - pt.y)/(center.x - pt.x)) * (180 / math.pi)
+        return 270 + math.atan((center[1] - pt[1])/(center[0] - pt[0])) * (180 / math.pi)
 
 
 def get_distance(a, b):
-    return math.sqrt((a.x - b.x)**2 + (a.y - b.y)**2)
+    return np.linalg.norm(a - b)
 
 
 def pt_on_table(pt):
     return (pt.x > (38/SCALE) and pt.x < (661/SCALE)) and (pt.y > (38/SCALE) and pt.y < (347/SCALE))
 
 
-def clear_table():
-    for i in range(16):
-        entity = world[i]
-        box.remove_body(i)
-        entity.y = be.y
-        entity.x = be.x + (2 * i * ball_radius) + (2 * ball_radius)
-        box.add_body(entity)
+#def clear_table():
+    #for i in range(16):
+        #entity = world[i]
+        #box.remove_body(i)
+        #entity.y = BE.y
+        #entity.x = BE.x + (2 * i * BALL_RADIUS) + (2 * BALL_RADIUS)
+        #box.add_body(entity)
 
 
 def rack_8ball():
     for i in range(16):
         entity = world[i]
         box.remove_body(i)
-        entity.pos = eight_ball_locs[i]
+        entity.pos = array(eight_ball_locs[i])
         entity.on_table = True
         box.add_body(entity)
 
 
+def draw_line(start, end, color, width, alpha=0.8):
+    glEnable(GL_BLEND)
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
+
+    glLineWidth(width)
+    pyglet.gl.glColor4f(*(color / 255.0), alpha)
+    line = [int(i) for i in (*start, *end)]
+    pyglet.graphics.draw(2, pyglet.gl.GL_LINES, ('v2i', line))
+
+    glDisable(GL_BLEND)
+    pyglet.gl.glColor4f(1, 1, 1, 1)
+
+
 class GameCore:
     def __init__(self):
-        self.width = canvas_width
-        self.height = canvas_height
+        self.width = CANVAS_WIDTH
+        self.height = CANVAS_HEIGHT
         self.is_running = False
 
-    def update(self, elapsed_time):
-        box.update()
-        bodies_state = box.get_state()
-        for bid in bodies_state:
-            entity = world[id]
-            if entity is not None:
-                entity.update(bodies_state)
-                for pt in goal_pts:
-                    if intersect(entity, pt, 0.2):
-                        entity.dead = True
-                        try:
-                            box.remove_body(bid)
-                            entity.pos = be.pos
-                            entity.on_table = False
-                            box.add_body(entity)
-                            box.apply_impulse(bid, 0, 2)
-                        except Exception as e:
-                            print(e)
+    #def update(self, elapsed_time):
+        #box.update()
+        #bodies_state = box.get_state()
+        #for bid in bodies_state:
+            #entity = world[bid]
+            #if entity is not None:
+                #entity.update(bodies_state)
+                #for pt in GOAL_PTS:
+                    #if intersect(entity, pt, 0.2):
+                        #entity.dead = True
+                        #try:
+                            #box.remove_body(bid)
+                            #entity.pos = BE.pos
+                            #entity.on_table = False
+                            #box.add_body(entity)
+                            #box.apply_impulse(bid, 0, 2)
+                        #except Exception as e:
+                            #print(e)
 
     def render(self):
         self.window.clear()
-        glClearColor(*clear_color)
+        glClearColor(*CLEAR_COLOR)
         glEnable(GL_BLEND)
         glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA)
         self.back_img.blit(0, 0)
+
+        if ball_to_hit is not None and mouse_down_pt is not None:
+            imp_perc = min(get_distance(ball_to_hit.pos, mouse_down_pt) * 3,
+                           MAX_IMPULSE) * 1.0 / MAX_IMPULSE
+            color_fade = get_color_fade(STICK_START_COLOR, STICK_END_COLOR, imp_perc)
+            draw_line(ball_to_hit.pos * SCALE, mouse_down_pt * SCALE,
+                      color_fade, CUE_LINE_WIDTH)
 
         for k, entity in world.items():
             if not entity.hidden:
@@ -431,38 +462,6 @@ class GameCore:
         self.window.dispatch_events()
         self.window.flip()
 
-    def draw(self, ctx):
-        ctx.line_width = 1
-        ctx.clear_rect(0, 0, this.width, this.height)
-        ctx.draw_image(back_img, 0, 0, self.width, table_height)
-
-        if ball_to_hit and mouse_down_pt:
-            imp_perc = math.min(get_distance(ball_to_hit, mouse_down_pt) * 3,
-                                max_impulse) * 1.0 / max_impulse
-            color_fade = get_color_fade(stick_start_color, stick_end_color, imp_perc)
-            line_width = ctx.line_width
-            ctx.line_width = 3
-            ctx.begin_path()
-            ctx.move_to(ball_to_hit * SCALE)
-            ctx.line_to(mouse_down_pt * SCALE)
-            ctx.stroke_style = RGB(r=color_fade.r, g=color_fade.g, b=color_fade.b)
-            ctx.stroke()
-            ctx.close_path()
-            ctx.line_width = line_width
-
-        for entity in world:
-            if not entity.hidden:
-                entity.draw(ctx, SCALE)
-
-        if ball_to_move and mouse_down_pt:
-            line_width = ctx.line_width
-            ctx.line_width = 5
-            ctx.stroke_style = RGB(0, 0, 0, 0.5)
-            ctx.arc(mouse_down_pt * SCALE, ball_radius * SCALE, 0, math.pi * 2, True)
-            ctx.close_path()
-            ctx.stroke()
-            ctx.line_width = line_width
-
     def init(self):
         self.canvas = None
         config = pyglet.gl.Config(sample_buffers=1, samples=4, double_buffer=True)
@@ -470,6 +469,50 @@ class GameCore:
                                            height=self.height, display=None,
                                            config=config, resizable=True)
         self.window.on_draw = self.on_draw
+        self.window.on_mouse_press = self.on_mouse_press
+        self.window.on_mouse_release = self.on_mouse_release
+        self.window.on_mouse_drag = self.on_mouse_drag
+
+    def on_mouse_drag(self, x, y, dx, dy, buttons, modifiers):
+        global mouse_down_pt
+        if mouse_down_pt is not None:
+            mouse_down_pt = get_gfx_mouse(x, y)
+
+    def on_mouse_press(self, x, y, button, modifiers):
+        global selected_ball, mouse_down_pt, ball_to_hit
+
+        for k, entity in world.items():
+            entity.selected = False
+
+        pt = get_gfx_mouse(x, y)
+        mouse_down_pt = pt
+        selected_ball = get_collided_ball(pt)
+        if selected_ball is not None:
+            if selected_ball.on_table:
+                ball_to_hit = selected_ball
+                ball_to_hit.selected = True
+
+    def on_mouse_release(self, x, y, button, modifiers):
+        global ball_to_move, ball_to_hit
+
+        pt = get_gfx_mouse(x, y)
+
+        if ball_to_hit is not None:
+            degrees = get_degrees(ball_to_hit.pos, pt)
+            box.apply_impulse(ball_to_hit.id, degrees + 90,
+                              min(get_distance(ball_to_hit.pos, pt) * 3,
+                                  MAX_IMPULSE))
+            ball_to_hit = None
+
+        if ball_to_move is not None:
+            if pt_on_table(pt):
+                box.remove_body(ball_to_move.id)
+                ball_to_move.pos = pos
+                ball_to_move.on_table = True
+                box.add_body(ball_to_move)
+            ball_to_move = None
+
+
 
     def load_resources(self, canvas):
         self.back_img = pyglet.image.load('pool_table_700x385.png')
@@ -480,6 +523,24 @@ class GameCore:
         self.launch_loop()
         while True:
             self.render()
+            box.update()
+            bodies_state = box.get_state()
+            for bid in bodies_state:
+                entity = world[bid]
+                if entity is not None:
+                    entity.update(bodies_state[bid])
+                    for pt in GOAL_PTS:
+                        if intersect(entity.pos, pt, 0.2):
+                            entity.dead = True
+                            try:
+                                box.remove_body(bid)
+                                entity.pos = BE
+                                entity.on_table = False
+                                box.add_body(entity)
+                                box.apply_impulse(bid, 0, 2)
+                            except Exception as e:
+                                print(e)
+
 
     def stop(self):
         self.is_running = False
@@ -498,18 +559,17 @@ def main():
     global game, box
     for i, iS in enumerate(initial_state):
         if i < 16:
-            iS.radius = ball_radius
+            iS.radius = BALL_RADIUS
             iS.linear_damping = 0.6
             iS.angular_damping = 0.5
-            iS.reset_shots = 0.9
+            iS.restitution = 0.9
 
         if hasattr(iS, 'points'):
             for j in range(len(iS.points)):
                 iS.points[j] = iS.points[j] / SCALE
 
         if i > 15 and i < 28:
-            iS.x = 0
-            iS.y = 0
+            iS.pos = array([0, 0])
 
         world[i] = iS
 
