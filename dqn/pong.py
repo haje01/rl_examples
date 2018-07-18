6311b695c2f8116239d7a82d2e52c433c0887fe4
JeongJu Kim <haje01@gmail.com>
2018-07-18 13:17:27 +0900

    change model

--- dqn/pong.py
+++ dqn/pong.py
@@ -54,29 +54,61 @@ resize = T.Compose([
                    ])
 
 
+# class DQN(nn.Module):
+#     """Deep Q-Network."""
+
+#     def __init__(self, action_size):
+#         """init."""
+#         super(DQN, self).__init__()
+#         self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
+#         self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
+#         self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
+#         self.fc1 = nn.Linear(64 * 7 * 7, 512)
+#         self.fc2 = nn.Linear(512, ACTION_SIZE)
+
+#     def forward(self, x):
+#         """전방 연쇄."""
+#         # PyTorch 입력은 Batch, Channel, Height, Width 를 가정
+#         x = F.relu(self.conv1(x))
+#         x = F.relu(self.conv2(x))
+#         x = F.relu(self.conv3(x))
+#         # 첫 번째 배치 사이즈는 그대로 이용하고, 나머지는 as is로 flatten
+#         x = x.view(x.size(0), -1)
+#         x = F.relu(self.fc1(x))
+#         x = self.fc2(x)  # 최종 출력은 Q값이기에 soft-max 쓰지 않음.
+#         return x
+
 class DQN(nn.Module):
     """Deep Q-Network."""
 
-    def __init__(self, action_size):
+    def __init__(self, input_shape, n_actions):
         """init."""
         super(DQN, self).__init__()
-        self.conv1 = nn.Conv2d(4, 32, kernel_size=8, stride=4)
-        self.conv2 = nn.Conv2d(32, 64, kernel_size=4, stride=2)
-        self.conv3 = nn.Conv2d(64, 64, kernel_size=3, stride=1)
-        self.fc1 = nn.Linear(64 * 7 * 7, 512)
-        self.fc2 = nn.Linear(512, ACTION_SIZE)
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(input_shape[0], 32, kernel_size=8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, kernel_size=3, stride=1),
+            nn.ReLU()
+        )
+
+        conv_out_size = self._get_conv_out(input_shape)
+        self.fc = nn.Sequential(
+            nn.Linear(conv_out_size, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions)
+        )
+
+    def _get_conv_out(self, shape):
+        o = self.conv(torch.zeros(1, *shape))
+        return int(np.prod(o.size()))
 
     def forward(self, x):
         """전방 연쇄."""
-        # PyTorch 입력은 Batch, Channel, Height, Width 를 가정
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        # 첫 번째 배치 사이즈는 그대로 이용하고, 나머지는 as is로 flatten
-        x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
-        x = self.fc2(x)  # 최종 출력은 Q값이기에 soft-max 쓰지 않음.
-        return x
+        conv_out = self.conv(x).view(x.size()[0], -1)
+        return self.fc(conv_out)
 
 
 def save_batch_images(histories):
@@ -90,11 +122,13 @@ def save_batch_images(histories):
 class DQNAgent:
     """DQN 에이전트."""
 
-    def __init__(self, device):
+    def __init__(self, env, device):
         """초기화."""
-        self.net = DQN(action_size=ACTION_SIZE).to(device)
-        self.net.apply(self.weights_init)
-        self.target_net = DQN(action_size=ACTION_SIZE).to(device)
+        self.net = DQN(env.observation_space.shape, ACTION_SIZE)\
+            .to(device)
+        # self.net.apply(self.weights_init)
+        self.target_net = DQN(env.observation_space.shape, ACTION_SIZE)\
+            .to(device)
         self.update_target_net()
         self.eps = 1.0
         self.eps_start, self.eps_end = 1.0, EGREEDY_END_EPS
@@ -165,9 +199,12 @@ class DQNAgent:
 
     def train_code2(self):
         """Ptan 책 학습 코드."""
+        self.optimizer.zero_grad()
         # 배치 데이터 샘플링
         batch = list(zip(*random.sample(self.memory, BATCH_SIZE)))
         states, actions, rewards, next_states, dones = batch
+        states = np.array(states)
+        next_states = np.array(next_states)
 
         states_v = torch.tensor(states).to(device)
         next_states_v = torch.tensor(next_states).to(device)
@@ -207,12 +244,12 @@ class DQNAgent:
             rewards.append(sample[2])
             deads.append(sample[4])
 
-        # nump -> torch FloatTensor로 변환
-        histories = torch.from_numpy(histories).float()
-        next_histories = torch.from_numpy(next_histories).float()
+        # numpy -> torch FloatTensor로 변환
+        histories = torch.from_numpy(histories).float().to(device)
+        next_histories = torch.from_numpy(next_histories).float().to(device)
 
         # 모델에서 이번 이력의 동작 가치를 예측
-        actions = torch.LongTensor(actions).unsqueeze(1)
+        actions = torch.LongTensor(actions).unsqueeze(1).to(device)
         # 이력으로 동작 가치(Q) 배열 예측 후, 동작을 인덱스로 동작 가치를 얻음
         q_values = self.net(histories).gather(1, actions).squeeze(-1)
 
@@ -228,7 +265,7 @@ class DQNAgent:
                 targets[i] = rewards[i]
             else:
                 targets[i] = rewards[i] + GAMMA * target_values[i]
-        targets = Variable(torch.from_numpy(targets)).float()
+        targets = Variable(torch.from_numpy(targets)).float().to(device)
 
         # 모델과 타겟 모델에서 예측한 Q밸류의 차이가 손실
         # (Hubber 손실)
@@ -261,7 +298,7 @@ def init_env():
 def train(device):
     """학습."""
     env = init_env()
-    agent = DQNAgent(device)
+    agent = DQNAgent(env, device)
     global_step = prev_step = 0
     epstart = None
     elapsed = speed = 0
@@ -369,7 +406,7 @@ def train(device):
 def play(device):
     """플레이."""
     env = init_env()
-    agent = DQNAgent(device)
+    agent = DQNAgent(env, device)
     agent.net.load_state_dict(torch.load(LOAD_MODEL))
     global_step = 0
 
